66cd7f7137b9fe3017325d5e6c0e3967057fe86d
ujkdb <ujksitdb@gmail.com>
2021-04-20 22:21:06 +0300

    примитивные формы без базы данных

---
+++ forms/user.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, BooleanField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+
+
+class RegisterForm(FlaskForm):
+    name = StringField('Имя', validators=[DataRequired()])
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, redirect, request, abort
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 from data import db_session
+from forms.user import RegisterForm, LoginForm
 
 app = Flask(__name__)
 login_manager = LoginManager()
@@ -25,18 +26,40 @@ def index():
     return render_template("base.html")
 
 
-@app.route('/login', methods=['GET'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login_form.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        """db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)"""
+        return redirect("/")
+        return render_template('login.html', message="Неправильный логин или пароль", form=form)
+    return render_template('login_form.html', title='Авторизация', form=form)
 
 
-@app.route('/post', methods=['GET'])
-def post():
-    response = requests.get("http://127.0.0.1:5000/login", params={'email': 'email', "password": "password"})
-    json_response = response.json()
-    repository = json_response["email"]
-    print(repository)
-    return render_template('base.html')
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        """if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            hashed_password=form.password.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()"""
+        return redirect('/')
+    return render_template('register_form.html', title='Регистрация', form=form)
 
 
 if __name__ == '__main__':
