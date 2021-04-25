9a0ea447e3c6ac05ed6e855914233f095470452a
Leamich <mishak2005med@mail.ru>
2021-04-25 22:00:49 +0400

    Статистика и ранговая система

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def show_user(user_id):
     your_account = current_user.is_authenticated and current_user.id == user_id
 
     return render_template('show_user.html', username=user.name, files=files,
-                           your_account=your_account)
+                           your_account=your_account, pic=user.get_rank_picture())
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -140,13 +140,8 @@ def draw_task():
                                    fact=task.description,
                                    error_message="Неверный формат")
         db_sess = db_session.create_session()
-        if db_sess.query(Picture).filter(Picture.name == filename).first():
-            return render_template('draw_task.html', form=form, difficulty=difficulty,
-                                   image_cap=task.image,
-                                   caption=task.name,
-                                   fact=task.description,
-                                   error_message="Вы уже отправляли этот файл")
-        if db_sess.query(Picture).filter(Picture.task_id == task.id).first():
+        if db_sess.query(Picture).filter(
+                Picture.task_id == task.id).filter(Picture.owner_id == current_user.id).first():
             return render_template('draw_task.html', form=form, difficulty=difficulty,
                                    image_cap=task.image,
                                    caption=task.name,
@@ -161,12 +156,20 @@ def draw_task():
         db_sess.commit()
 
         picture.name = str(picture.id) + get_file_extension(picture.name)
+
+        score = current_user.score.split(';')
+        score[task.difficulty] = str(int(score[task.difficulty]) + 1)
+        current_user.score = ';'.join(score)
+        current_user.check_rank()
+
+        db_sess.merge(current_user)
         db_sess.merge(picture)
         db_sess.commit()
 
         f.save(os.path.join(
-            'static/users_pictures', picture.name
+            'static/users_pictures/', picture.name
         ))
+
         return render_template('draw_task.html', form=form, difficulty=difficulty,
                                image_cap=task.image,
                                caption=task.name,
@@ -246,6 +249,9 @@ def check_random():
 
 @app.route('/close_server')
 def stop_server():
+    if not (current_user.is_authenticated and current_user.name == 'admin'):
+        return 'You can\'t access to this page'
+
     sys.exit()
 
 
@@ -257,6 +263,46 @@ def authors_information():
 @app.route('/license_information')
 def license_information():
     return render_template('license_information.html')
+
+
+@app.route('/statistics')
+def stats():
+    if not current_user.is_authenticated:
+        return redirect('/login')
+
+    score_num = current_user.get_score_num()
+
+    easy, medium, hard = map(int, current_user.score.split(';'))
+    total = easy + medium + hard
+
+    prefers = current_user.prefer_difficulty()
+    len_prefers = sum(map(lambda x: 1 if prefers[x] else 0, prefers.keys()))
+    if len_prefers == 3:
+        prefers = 'У вас не наблюдается приверженности какой-то конкретной ' \
+                  'сложности. \nВаш баланс вызывает зависть даже у Таноса.'
+    elif len_prefers == 2:
+        if prefers['easy'] and prefers['medium']:
+            prefers = 'У вас наблюдается склонность к лёгкой и средней сложности. \n' \
+                      'Следующим этапом, должно быть, будет сложная?'
+        elif prefers['easy'] and prefers['hard']:
+            prefers = 'У вас наблюдается склонность к лёгкой и сложной сложности. \n' \
+                      'Вы — человек контрастов.'
+        else:
+            prefers = 'У вас наблюдается склонность к средней и сложной сложности. \n' \
+                      'Очевидно, что вы профессионал, но не стоит ' \
+                      'пренебрегать лёгкой сложностью — она тоже может Вас удивить.'
+    else:
+        prefers = {'easy': 'Вы склонны к выбору лёгкой сложности. Это прекрасно, Вам ещё есть куда расти!',
+                   'medium': 'Вы склонны к выбору средней сложности. \nНадеемся, '
+                             'Вы продолжите поднимать планку и ставить вызовы своему мастерству.',
+                   'hard': 'Вы склонны к выбору сложной сложности. \nНет сомнений, Вы — мастер. '
+                           'Благо, у вас ещё есть шанс поставить себе вызов, попробовав уровни пониже.'
+                   }[next(filter(lambda x: prefers[x], prefers.keys()))]
+
+    return render_template('statistics.html', score_num=score_num,
+                           procents=score_num * 10 // current_user.rank,
+                           easy=easy * 100 // total, medium=medium * 100 // total,
+                           hard=hard * 100 // total, prefers=prefers, post_scriptum=prefers)
 
 
 if __name__ == '__main__':
