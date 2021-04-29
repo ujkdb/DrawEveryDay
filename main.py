774379cff230db1519bcab9fb768e0b04c98448b
ujkdb <ujksitdb@gmail.com>
2021-04-29 17:36:45 +0400

    maybe ready to deploy

--- main.py
+++ main.py
@@ -442,7 +442,8 @@ def main():
     os.chdir(os.getcwd() + '/static/users_pictures')  # смена директории для галереи
 
     # запускаем приложение
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
